685f4e0d63ca82f7ffd4dd4dfe020a4923908499
Samir Rajani <rajanisamir@gmail.com>
2022-04-30 01:47:21 +0000

    cleaned up and documented code

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -54,22 +54,27 @@ class QLearning(object):
         self.robot_action_topic = "q_learning/robot_action"
         self.reward_topic = "q_learning/reward"
 
-        # Set up publishers and subscribers
-        self.q_matrix_pub = rospy.Publisher(self.q_matrix_topic, QMatrix, queue_size=10) # publisher for Q-matrix
-        self.robot_action_pub = rospy.Publisher(self.robot_action_topic, RobotMoveObjectToTag, queue_size=10) # publisher for robot action
-        self.reward_sub = rospy.Subscriber(self.reward_topic, QLearningReward, self.get_reward) # subscriber to reward topic
+        # Set up publishers for Q-matrix and robot actions and subscriber for rewards
+        self.q_matrix_pub = rospy.Publisher(self.q_matrix_topic, QMatrix, queue_size=10)
+        self.robot_action_pub = rospy.Publisher(self.robot_action_topic, RobotMoveObjectToTag, queue_size=10)
+        self.reward_sub = rospy.Subscriber(self.reward_topic, QLearningReward, self.get_reward)
 
-        rospy.sleep(5)
+        # Give publisher enough time to set up
+        rospy.sleep(1)
 
-        print("slept")
-        # Initialize parameters associated with Q-learning
+        # Initialize parameters associated with Q-learning:
+            # alpha: a value in (0, 1] representing the learning rate of the Q-learning algorithm
+            # gamma: a value in [0, 1) representing the discount factor for future rewards in the Q-learning algorithm
+            # convergence_check_threshold: the sum of rewards after which the Q-learning algorithm will check the
+            #   Q-matrix for convergence
+            # convergence_threshold: the sum of the element-wise absolute differences between the current Q-matrix and
+            #   the Q-matrix at the previous convergence check below which the Q-matrix will be considered to have converged
         self.alpha = 1
         self.gamma = 0.8
-        # self.convergence_check_steps = 100 #50
-        self.convergence_check_threshold = 5000
+        self.convergence_check_threshold = 1500
         self.convergence_threshold = 5
 
-        # initialize, learn, and save Q-matrix  
+        # Initialize, learn, and save Q-matrix  
         self.initialize_q_matrix()
         self.learn_q_matrix()
         self.save_q_matrix()
@@ -79,100 +84,91 @@ class QLearning(object):
         self.q_matrix = np.zeros((len(self.states), len(self.actions)))
         self.publish_q_matrix()
     
-    # publishes the Q-matrix
+    # publishes the current Q-matrix
     def publish_q_matrix(self):
+        # add header to Q-matrix containing a timestamp and ID
         q_matrix_stamped = QMatrix()
         q_matrix_stamped.header = Header(stamp=rospy.Time.now(), frame_id=self.q_matrix_topic)
         q_matrix_stamped.q_matrix = self.q_matrix.tolist()
 
         self.q_matrix_pub.publish(q_matrix_stamped)
 
-    # callback function for reward subscriber
+    # Callback function for the reward subscriber, which sets self.current_reward to the received reward
     def get_reward(self, data):
-        print("Received reward: ", data.reward)
         self.curr_reward = data.reward
 
-    # determines if self.q_matrix has converged with prev_q_matrix
+    # Determines if self.q_matrix has converged with prev_q_matrix
     def q_matrix_has_converged(self, prev_q_matrix):
+        # Computes the sum of the element-wise absolute difference between current Q-matrix and previous Q-matrix;
+        #   return if the sum is below the convergence threshold
         diff = abs(self.q_matrix - prev_q_matrix)
         return np.sum(diff) < self.convergence_threshold
 
-    # publishes action to execute
+    # Publishes a specified action for the robot to execute
     def publish_action(self, action):
         action_stamped = RobotMoveObjectToTag()
         action_stamped.robot_object = action['object']
         action_stamped.tag_id = action['tag']
         self.robot_action_pub.publish(action_stamped)
     
-    #update current state 
-    def update_state(self, state, action):
+    # Returns the state associated with taking a specified action from a specified state
+    def get_state(self, state, action):
         for s, a in enumerate(self.action_matrix[state]):
             if a == action:
-                print("state", s)
                 return s
 
-    def get_possible_actions(self, curr_state):
+    # Returns a list of actions possible from a specified state
+    def get_possible_actions(self, state):
         possible_actions = []
-        for action in self.action_matrix[curr_state]:
+        for action in self.action_matrix[state]:
             if action != -1:
                 possible_actions.append(action)
         return possible_actions
             
-    # executes the Q-learning algorithm
+    # Executes the Q-learning algorithm
     def learn_q_matrix(self):
         t = 0
         prev_q_matrix = None
         curr_state = 0
+        curr_reward_sum = 0
         self.curr_reward = None
-        self.total_reward = 0
+
         while True:
-            # if sufficiently many steps have passed, check for convergence
-            if self.total_reward > 5000:
+            # If the sum of the rewards received since the last convergence check exceeds a threshold,
+            #   check for convergence.
+            if curr_reward_sum > self.convergence_check_threshold:
                 if prev_q_matrix is not None and self.q_matrix_has_converged(prev_q_matrix):
                     break
                 prev_q_matrix = self.q_matrix
-                self.total_reward = 0
+                curr_reward_sum = 0
 
-                print("Unconverged Matrix: ", self.q_matrix)
-
-            # choose an action at random
+            # Choose and publish an action at random; reset the state to 0 if there are none available.
             possible_actions = self.get_possible_actions(curr_state)
-            print("Possible Actions: ", possible_actions)
             if len(possible_actions) == 0:
-                print("No Possible Actions... Resetting State")
                 curr_state = 0
                 possible_actions = self.get_possible_actions(curr_state)
-
             chosen_action = int(choice(possible_actions))
-            print("Executing Action: ", chosen_action)
             self.publish_action(self.actions[chosen_action])
 
-            updated_state = self.update_state(curr_state, chosen_action)
+            updated_state = self.get_state(curr_state, chosen_action)
 
             while self.curr_reward is None:
                 pass
 
-            # update q_matrix
-            self.q_matrix[curr_state][chosen_action] += self.alpha * (self.curr_reward + self.gamma * max(self.q_matrix[updated_state]) - self.q_matrix[curr_state][chosen_action])
+            # Update Q-matrix using the formula
+            self.q_matrix[curr_state][chosen_action] += self.alpha * (self.curr_reward + \
+                self.gamma * max(self.q_matrix[updated_state]) - self.q_matrix[curr_state][chosen_action])
             
-            self.total_reward += self.curr_reward
+            curr_reward_sum += self.curr_reward
             self.curr_reward = None
             
-            print("t: ", t)
-            print("Updated q: ", self.q_matrix[curr_state][chosen_action])
-            # increment time step
+            # Increment time step and update robot's state
             t += 1
-
-            #update state
             curr_state = updated_state
-            print("updated state to ", curr_state)
 
-            # publish q-matrix
+            # Publish q-matrix
             self.publish_q_matrix()
         
-        print("done")
-        print(self.q_matrix)
-
     def save_q_matrix(self):
         np.savetxt('q_matrix.csv', self.q_matrix, fmt='%.2f', delimiter=',')
 
