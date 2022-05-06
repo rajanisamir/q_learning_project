2544bc3a4f419cd74c02e6696ab18e65aea7aac0
Samir Rajani <rajanisamir@gmail.com>
2022-05-06 17:11:50 +0000

    full pick up put down sequence working

--- scripts/perform_actions.py
+++ scripts/perform_actions.py
@@ -58,16 +58,17 @@ class PerformActions(object):
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.process_scan)
 
         # Set up image recognition parameters
-        self.object_center_x = None
-        self.object_center_y = None
+        self.target_center_x = None
+        self.target_center_y = None
         self.img_center_x = None
         self.img_center_y = None
 
-        # Set up proportional control parameters
+        # Set up proportional control parameters for tag movement
         self.k_p_ang_cam = 0.005
         self.k_p_ang_scan = 0.7
-        self.k_p_lin = 0.05
+        self.k_p_lin = 0.1
         self.desired_obj_distance = 0.18
+        self.desired_tag_distance = 0.4
         self.drive_error_pixels = 30
         self.switch_to_scan = 10
         self.pickup_angle_tolerance = 0.003 # IN RADIANS (< 1 DEGREE)
@@ -110,42 +111,34 @@ class PerformActions(object):
 
             else:
 
-                if self.object_center_x is None:
-
+                if self.target_center_x is None:
+                    print('searching...')
                     self.twist.linear.x = 0
                     self.twist.angular.z = self.angular_search_velocity
 
                 else:
 
-                    object_center_x_error = self.img_center_x - self.object_center_x
-                    self.twist.angular.z = self.k_p_ang_cam * object_center_x_error
+                    target_center_x_error = self.img_center_x - self.target_center_x
+                    self.twist.angular.z = self.k_p_ang_cam * target_center_x_error
 
-                    # obj_distances = list(filter(lambda dist: dist != 0, data.ranges[-60:60]))
-                    # print(obj_distances)
-                    # if len(obj_distances) == 0:
-                    #     obj_distance = 1
-                    # else:
-                    #     obj_distance = min(obj_distances)
+                    target_distance = data.ranges[0] if data.ranges[0] != 0 else 2
+                    desired_target_distance = self.desired_obj_distance if self.goal == Goal.GO_TO_OBJECT else self.desired_tag_distance
+                    distance_error = target_distance - desired_target_distance
 
-                    obj_distance = data.ranges[0] if data.ranges[0] != 0 else 2
-                    
-                    print("closest object distance: ", obj_distance)
-                    distance_error = obj_distance - self.desired_obj_distance
-
-                    # TODO: Add case for going to tag.
-
-                    print("x center error: ", object_center_x_error)
-                    print("distance error: ", distance_error)
-
-                    if self.goal == Goal.GO_TO_OBJECT and abs(object_center_x_error) < self.switch_to_scan and abs(distance_error) < 0.1:
-                        print('at object')
-                        self.at_object = True
-                        
-                    if abs(object_center_x_error) < self.drive_error_pixels:
+                    if abs(target_center_x_error) < self.drive_error_pixels:
                         self.twist.linear.x = self.k_p_lin * distance_error
                     else:
                         self.twist.linear.x = 0
 
+                    if self.goal == Goal.GO_TO_OBJECT and abs(target_center_x_error) < self.switch_to_scan and abs(distance_error) < 0.1:
+                        self.at_object = True
+                    
+                    if self.goal == Goal.GO_TO_TAG and abs(distance_error) < 0.1:
+                        print('finished going to tag')
+                        self.goal = Goal.PUT_DOWN
+                        self.twist.linear.x = 0
+                        self.twist.angular.z = 0
+                    
         # Clamp angular speed to 1.82 rad/s and linear speed to 0.26 m/s
         #   to ensure we don't exceed the maximum velocity of the Turtlebot.
         self.twist.angular.z = min(1.82, max(self.twist.angular.z, -1.82))
@@ -171,36 +164,74 @@ class PerformActions(object):
         # if there are any specified color pixels found
         if M['m00'] > 10:
                 # center of the color pixels in the image
-                self.object_center_x = int(M['m10']/M['m00'])
-                self.object_center_y = int(M['m01']/M['m00'])
-                print("object_center_x:", self.object_center_x)
-                print("object_center_y:", self.object_center_y)
+                self.target_center_x = int(M['m10']/M['m00'])
+                self.target_center_y = int(M['m01']/M['m00'])
+                print("target_center_x:", self.target_center_x)
+                print("target_center_y:", self.target_center_y)
         
         else:
-            self.object_center_x = None
-            self.object_center_y = None
-
-        img_width, img_height, img_depth = img.shape
-        self.img_center_x = img_width / 2
-        self.img_center_y = img_height / 2
+            self.target_center_x = None
+            self.target_center_y = None
 
         # shows the debugging window
         # hint: you might want to disable this once you're able to get a red circle in the debugging window
         # cv2.imshow("window", img)
         # cv2.waitKey(3)
+    
+    def tag_handler(self, img):
+        self.tag_id = 1
 
+        # turn the image into a grayscale
+        grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+        corners, ids, rejected_points = cv2.aruco.detectMarkers(grayscale_img, aruco_dict)
+        
+        if ids is None:
+            self.target_center_x = None
+            self.target_center_y = None
+            print('no tags found')
+            return
+
+        ids = np.ndarray.flatten(ids)
+        tag_idx = None
+        for idx, id in enumerate(ids):
+            if id == self.tag_id:
+                tag_idx = idx
+
+        if tag_idx is None:
+            self.target_center_x = None
+            self.target_center_y = None
+            print('no tag matches found')
+            return
+        
+        x_coords = []
+        y_coords = []
+        for coord in corners[tag_idx][0]:
+            x_coords.append(coord[0])
+            y_coords.append(coord[1])
+
+        tag_center_x = sum(x_coords) / len(x_coords)
+        tag_center_y = sum(y_coords) / len(y_coords)
+
+        self.target_center_x = tag_center_x
+        self.target_center_y = tag_center_y
+        
 
     #handle 2 statuses: go_to_color and go_to_tag
     def process_image(self, data):
 
         img = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-        
+
+        img_width, img_height, img_depth = img.shape
+        self.img_center_x = img_width / 2
+        self.img_center_y = img_height / 2
+
         if self.goal == Goal.GO_TO_OBJECT:
             self.color_object_handler(img)
 
         elif self.goal == Goal.GO_TO_TAG:
-            # TODO
-            pass
+            self.tag_handler(img)
 
     
     def perform_action(self, data):
@@ -209,7 +240,6 @@ class PerformActions(object):
         self.color = data.robot_object
         self.tag_id = data.tag_id
 
-        
         # TODO: Move to colored object (maybe sleep here so that have time for robot to go to object)
         
         # Pick up object
@@ -220,19 +250,22 @@ class PerformActions(object):
         print('started picking up')
         rospy.sleep(18) # change this depending on how long it takes to pick up an object, or create action completion pub
         
-        return
+        self.at_object = False # stop using scan data
+
+        # TODO: BACK UP AND SPIN
 
         self.goal = Goal.GO_TO_TAG
 
-        # TODO: Move to tag
-
-        self.goal = Goal.PUT_DOWN
-
         # Put down object
+        while not self.goal == Goal.PUT_DOWN:
+            pass
+
         self.arm_action_pub.publish("put_down")
         print('started putting down')
         rospy.sleep(18) # change this depending on how long it takes to put down an object, or create action completion pub
         
+        # TODO: BACK UP AND SPIN
+
         self.goal = Goal.GO_TO_OBJECT
 
         self.action_completion_pub.publish(data)
